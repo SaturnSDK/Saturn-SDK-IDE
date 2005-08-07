e9f7d3d4febb707512cd4f69577be885c075e7a1
rickg22 <rickg22>
2005-08-07 00:51:11 +0000

    Converted files in "src/src" to unicode

--- src/src/dlgabout.cpp
+++ src/src/dlgabout.cpp
@@ -28,14 +28,14 @@
 #include "../sdk/licenses.h"
 #include "../sdk/configmanager.h"
 
-#define DESCR		"Welcome to "APP_NAME" v"APP_VERSION"!\n" \
+#define DESCR		_("Welcome to "APP_NAME" v"APP_VERSION"!\n" \
 					APP_NAME" is a full-featured IDE (Integrated Development Environment) " \
 					"aiming to make the individual developer (and the development team) " \
 					"work in a nice programming environment offering everything he/they " \
 					"would ever need from a program of that kind.\n" \
 					"Its pluggable architecture allows you, the developer, to add " \
 					"any kind of functionality to the core program, through the use of " \
-					"plugins...\n"
+					"plugins...\n")
 
 // class constructor
 dlgAbout::dlgAbout(wxWindow* parent)
@@ -43,10 +43,10 @@ dlgAbout::dlgAbout(wxWindow* parent)
 	wxXmlResource::Get()->LoadDialog(this, parent, _("dlgAbout"));
 
 	wxBitmap bmp;
-	wxString file = ConfigManager::Get()->Read("data_path") + "/images/splash.png"; 
+	wxString file = ConfigManager::Get()->Read(_T("data_path")) + _T("/images/splash.png"); 
 	bmp.LoadFile(file, wxBITMAP_TYPE_PNG);
 	XRCCTRL(*this, "lblTitle", wxStaticBitmap)->SetBitmap(bmp);
-	XRCCTRL(*this, "lblBuildTimestamp", wxStaticText)->SetLabel("Build: "APP_BUILD_TIMESTAMP);
+	XRCCTRL(*this, "lblBuildTimestamp", wxStaticText)->SetLabel(wxString(_T("Build: ")) + APP_BUILD_TIMESTAMP);
 	XRCCTRL(*this, "txtDescription", wxTextCtrl)->SetValue(DESCR);
 	XRCCTRL(*this, "txtThanksTo", wxTextCtrl)->SetValue(
         _("The wxWidgets project (http://www.wxwidgets.org)\n"
@@ -59,6 +59,8 @@ dlgAbout::dlgAbout(wxWindow* parent)
         "\n"
         "Contributors (in no special order):\n"
         "-----------------------------------\n"
+        "byo, Tiwag,\n"
+        "elvstone, me22: Conversion to Unicode\n"
         "byo, cyberkoa : wxSmith RAD plugin (WIP)\n"
         "P. A. Jimenez : Help and AStyle plugins\n"
         "Hakki Dogusan : DigitalMars compiler support\n"
@@ -72,7 +74,7 @@ dlgAbout::dlgAbout(wxWindow* parent)
 	XRCCTRL(*this, "txtLicense", wxTextCtrl)->SetValue(LICENSE_GPL);
 	XRCCTRL(*this, "lblName", wxStaticText)->SetLabel(APP_NAME);
 	XRCCTRL(*this, "lblVersion", wxStaticText)->SetLabel(APP_ACTUAL_VERSION);
-	XRCCTRL(*this, "lblAuthor", wxStaticText)->SetLabel("Yiannis An. Mandravellos");
+	XRCCTRL(*this, "lblAuthor", wxStaticText)->SetLabel(_("Yiannis An. Mandravellos"));
 	XRCCTRL(*this, "lblEmail", wxStaticText)->SetLabel(APP_CONTACT_EMAIL);
 	XRCCTRL(*this, "lblWebsite", wxStaticText)->SetLabel(APP_URL);
 }
