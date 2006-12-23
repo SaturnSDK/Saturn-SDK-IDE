197b67988c449385ca89badcdb95d069fb78e0b0
byo <byo>
2006-12-23 13:00:58 +0000

    Updated update.bat - it now copy all wxsmith's icons to devel and output folders

--- update.bat
+++ update.bat
@@ -2,8 +2,9 @@
 cls
 zip ..\..\..\devel\share\CodeBlocks\wxsmith.zip manifest.xml
 zip ..\..\..\output\share\CodeBlocks\wxsmith.zip manifest.xml
-REM md ..\..\..\devel\share\CodeBlocks\images\wxsmith > nul 2>&1
-REM md ..\..\..\output\share\CodeBlocks\images\wxsmith > nul 2>&1
-REM copy smithicons\*.png ..\..\..\devel\share\CodeBlocks\images\wxsmith\ > nul 2>&1
-REM copy smithicons\*.png ..\..\..\output\share\CodeBlocks\images\wxsmith\ > nul 2>&1
+md ..\..\..\devel\share\CodeBlocks\images\wxsmith > nul 2>&1
+md ..\..\..\devel\share\CodeBlocks\images\wxsmith > nul 2>&1
+md ..\..\..\output\share\CodeBlocks\images\wxsmith > nul 2>&1
+copy wxwidgets\icons\*.png ..\..\..\devel\share\CodeBlocks\images\wxsmith\ > nul 2>&1
+copy wxwidgets\icons\*.png ..\..\..\output\share\CodeBlocks\images\wxsmith\ > nul 2>&1
 exit 0
