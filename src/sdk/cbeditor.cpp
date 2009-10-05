2aed136d04afabb40c1471436b7aedafcdd0f8e9
jenslody <jenslody>
2009-10-05 21:26:32 +0000

    * dbg and propgrid test-branch: removed unnecessary code; added missing images for starthere page

--- src/sdk/cbeditor.cpp
+++ src/sdk/cbeditor.cpp
@@ -1405,7 +1405,6 @@ void cbEditor::InternalSetEditorStyleBeforeFileOpen(cbStyledTextCtrl* control)
     {
         control->SetMarginWidth(changebarMargin, 4);
         control->SetMarginType(changebarMargin,  wxSCI_MARGIN_SYMBOL);
-        control->SetMarginWidth(changebarMargin, 4);
         // use "|" here or we might break plugins that use the margin (none at the moment)
         control->SetMarginMask(changebarMargin, control->GetMarginMask(changebarMargin) |
                                                 (1 << wxSCI_MARKNUM_CHANGEUNSAVED) | (1 << wxSCI_MARKNUM_CHANGESAVED) );
@@ -2303,22 +2302,10 @@ void cbEditor::GotoPreviousChanged()
     }
 }
 
-void cbEditor::ShowChangebarMargin(bool show)
-{
-    cbAssert(GetControl());
-    GetControl()->SetMarginWidth(changebarMargin, show?4:0);
-}
-
 void cbEditor::SetChangeCollection(bool collectChange)
 {
     cbAssert(GetControl());
     GetControl()->SetChangeCollection(collectChange);
-}
-
-void cbEditor::SetScrollWidthTracking(bool trackWidth)
-{
-    cbAssert(GetControl());
-    GetControl()->SetScrollWidthTracking(trackWidth);
 }
 
 void cbEditor::Cut()
