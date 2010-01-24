a37e67a2e1b4e968cbd78969b63adda7e1217839
mortenmacfly <mortenmacfly>
2010-01-24 13:30:09 +0000

    * debugger branch: merged with trunk

--- src/plugins/contrib/codesnippets/settingsdlgform.h
+++ src/plugins/contrib/codesnippets/settingsdlgform.h
@@ -34,7 +34,7 @@
 class SettingsDlgForm : public wxScrollingDialog
 {
 	private:
-	
+
 	protected:
 		wxStaticText* m_staticText;
 		wxStaticText* m_staticText1;
@@ -47,7 +47,7 @@ class SettingsDlgForm : public wxScrollingDialog
 		wxRadioButton* m_RadioFloatBtn;
 		wxRadioButton* m_RadioDockBtn;
 		wxRadioButton* m_RadioExternalBtn;
-		
+
 		wxCheckBox* m_EditorsStayOnTopChkBox;
 		wxCheckBox* m_ToolTipsChkBox;
 		wxStaticText* m_CfgFolderStaticText;
@@ -55,13 +55,13 @@ class SettingsDlgForm : public wxScrollingDialog
 		wxStdDialogButtonSizer* m_sdbSizer;
 		wxButton* m_sdbSizerOK;
 		wxButton* m_sdbSizerCancel;
-	
+
 	public:
 		wxTextCtrl* m_CfgFolderTextCtrl;
 		wxTextCtrl* m_IniFolderTextCtrl;
 		SettingsDlgForm( wxWindow* parent, wxWindowID id = ID_DEFAULT, const wxString& title = wxEmptyString, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 462,389 ), long style = wxDEFAULT_DIALOG_STYLE|wxRESIZE_BORDER );
 		~SettingsDlgForm();
-	
+
 };
 
 #endif //__settingsdlgform__
