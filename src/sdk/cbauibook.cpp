0105e85d3ec75746c69371e51d779395145b1878
jenslody <jenslody>
2011-02-21 14:35:42 +0000

    * console-branch: merged with trunk (trunk to console-branch)

--- src/sdk/cbauibook.cpp
+++ src/sdk/cbauibook.cpp
@@ -60,6 +60,7 @@ cbAuiNotebook::cbAuiNotebook(wxWindow* pParent, wxWindowID id, const wxPoint& po
           m_pLastFocused(nullptr),
 #endif
           m_AllowToolTips(false),
+          m_OverTabCtrl(false),
           m_SetZoomOnIdle(false),
           m_IdNoteBookTimer(wxNewId())
 {
@@ -156,18 +157,13 @@ void cbAuiNotebook::ResetTabCtrlEvents()
         m_TabCtrls[i]->Disconnect(wxEVT_SIZE, wxSizeEventHandler(cbAuiNotebook::OnResize));
         m_TabCtrls[i]->Connect(wxEVT_SIZE, wxSizeEventHandler(cbAuiNotebook::OnResize));
         m_TabCtrls[i]->Disconnect(wxEVT_MOUSEWHEEL, wxMouseEventHandler(cbAuiNotebook::OnTabCtrlMouseWheel));
-#ifdef __WXMSW__
         m_TabCtrls[i]->Disconnect(wxEVT_ENTER_WINDOW, wxMouseEventHandler(cbAuiNotebook::OnEnterTabCtrl));
         m_TabCtrls[i]->Disconnect(wxEVT_LEAVE_WINDOW, wxMouseEventHandler(cbAuiNotebook::OnLeaveTabCtrl));
-#endif // #ifdef __WXMSW__
+        m_TabCtrls[i]->Connect(wxEVT_ENTER_WINDOW, wxMouseEventHandler(cbAuiNotebook::OnEnterTabCtrl));
+        m_TabCtrls[i]->Connect(wxEVT_LEAVE_WINDOW, wxMouseEventHandler(cbAuiNotebook::OnLeaveTabCtrl));
         if (s_AllowMousewheel)
         {
             m_TabCtrls[i]->Connect(wxEVT_MOUSEWHEEL, wxMouseEventHandler(cbAuiNotebook::OnTabCtrlMouseWheel));
-#ifdef __WXMSW__
-            // hack needed on wxMSW, because only focused windows get mousewheel-events
-            m_TabCtrls[i]->Connect(wxEVT_ENTER_WINDOW, wxMouseEventHandler(cbAuiNotebook::OnEnterTabCtrl));
-            m_TabCtrls[i]->Connect(wxEVT_LEAVE_WINDOW, wxMouseEventHandler(cbAuiNotebook::OnLeaveTabCtrl));
-#endif // #ifdef __WXMSW__
         }
     }
 }
@@ -231,6 +227,9 @@ void cbAuiNotebook::OnDwellTimerTrigger(wxTimerEvent& /*event*/)
 
     UpdateTabControlsArray();
 
+    if((m_pToolTip == nullptr) && !m_OverTabCtrl)
+        return;
+
     if (!s_UseTabTooltips || !m_AllowToolTips)
     {
         CancelToolTip();
@@ -284,8 +283,6 @@ void cbAuiNotebook::OnDwellTimerTrigger(wxTimerEvent& /*event*/)
     }
 }
 
-#ifdef __WXMSW__
-// hack needed on wxMSW, because only focused windows get mousewheel-events
 void cbAuiNotebook::OnEnterTabCtrl(wxMouseEvent& event)
 {
     if (!wxTheApp->IsActive())
@@ -297,6 +294,8 @@ void cbAuiNotebook::OnEnterTabCtrl(wxMouseEvent& event)
         cbAuiNotebook* nb = (cbAuiNotebook*)tabCtrl->GetParent();
         if (nb)
         {
+            nb->m_OverTabCtrl = true;
+#ifdef __WXMSW__
             if (   (nb->m_pToolTip == nullptr)
                 && (nb->m_pLastFocused == nullptr)
                 && (nb->m_LastSelected == wxNOT_FOUND) )
@@ -304,6 +303,7 @@ void cbAuiNotebook::OnEnterTabCtrl(wxMouseEvent& event)
                 nb->StoreFocus();
                 tabCtrl->SetFocus();
             }
+#endif // #ifdef __WXMSW__
         }
     }
 }
@@ -317,12 +317,19 @@ void cbAuiNotebook::OnLeaveTabCtrl(wxMouseEvent& event)
     if (tabCtrl)
     {
         cbAuiNotebook* nb = (cbAuiNotebook*)tabCtrl->GetParent();
-        if (nb && nb->m_pToolTip == nullptr)
-            nb->RestoreFocus();
+        if (nb)
+        {
+            nb->m_OverTabCtrl = false;
+#ifdef __WXMSW__
+            if (nb->m_pToolTip == nullptr)
+                nb->RestoreFocus();
+#endif // #ifdef __WXMSW__
+        }
     }
 
 }
 
+#ifdef __WXMSW__
 bool cbAuiNotebook::IsFocusStored(wxWindow* page)
 {
     wxWindow* win = m_pLastFocused;
@@ -428,7 +435,14 @@ void cbAuiNotebook::OnResize(wxSizeEvent& event)
     {
         cbAuiNotebook* nb = (cbAuiNotebook*)tabCtrl->GetParent();
         if (nb)
+        {
+            if(nb->m_TabCtrlSize == wxDefaultSize)
+                nb->m_TabCtrlSize = event.GetSize();
+            if(nb->m_TabCtrlSize == event.GetSize())
+                return;
+            nb->m_TabCtrlSize = event.GetSize();
             nb->MinimizeFreeSpace(tabCtrl);
+        }
     }
     event.Skip();
 }
@@ -502,19 +516,25 @@ void cbAuiNotebook::MinimizeFreeSpace(wxAuiTabCtrl* tabCtrl)
         // a little hacky, but it works
         wxPaintEvent event;
         tabCtrl->ProcessEvent(event);
+        int tabOffset = tabCtrl->GetTabOffset();
 
         wxClientDC dc(win);
         size_t lastTabIdx = tabCtrl->GetPageCount() - 1;
-        for (int i = lastTabIdx ; i >= 0; --i)
+
+        if(!tabCtrl->IsTabVisible(ctrl_idx, tabOffset, & dc, win))
         {
-            if (tabCtrl->IsTabVisible(ctrl_idx, i, & dc, win))
+            for (int i = lastTabIdx ; i >= 0; --i)
             {
-                while (i > 0 && tabCtrl->IsTabVisible(lastTabIdx, i-1, & dc, win))
-                    --i;
-                tabCtrl->SetTabOffset(i);
-                break;
+                if (tabCtrl->IsTabVisible(ctrl_idx, i, & dc, win))
+                {
+                    tabOffset = i;
+                    break;
+                }
             }
         }
+        while (tabOffset > 0 && tabCtrl->IsTabVisible(lastTabIdx, tabOffset-1, & dc, win))
+            --tabOffset;
+        tabCtrl->SetTabOffset(tabOffset);
     }
     tabCtrl->Refresh();
 }
