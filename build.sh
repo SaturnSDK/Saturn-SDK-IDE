783bc841fdd074935b26fa6cde65bc76937a9ddb
RedRingRico <teh_programerer@interactivelaboratories.com>
2014-10-03 16:52:30 +0100

    If the build fails, 1 is returned to the console

--- build.sh
+++ build.sh
@@ -4,6 +4,11 @@ export NCPU=`nproc`
 
 ./build-ide.sh
 
+if [ $? -ne 0 ]; then
+	echo "Failed building Code::Blocks"
+	exit 1
+fi
+
 
 if [[ "$CREATEINSTALLER" == "YES" ]]; then
 	./createinstaller.sh
