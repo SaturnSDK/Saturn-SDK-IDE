abeaa11f105ffb010c894a16ad56feec9ba42cfc
RedRingRico <teh_programerer@interactivelaboratories.com>
2014-10-15 12:45:08 +0100

    Removed the program prefix

--- build-ide.sh
+++ build-ide.sh
@@ -1,6 +1,6 @@
 mkdir build
 cd build
-../configure --host=$HOSTMACH --target=$TARGETMACH --build=$BUILDMACH --prefix=$INSTALLDIR
+../configure --host=$HOSTMACH --target=$TARGETMACH --build=$BUILDMACH --prefix=$INSTALLDIR --program-prefix=
 make -j${NPROC}
 make install -j${NPROC}
 
