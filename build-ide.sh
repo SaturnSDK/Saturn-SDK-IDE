fcbc73228455a467f06c46648be0e5d857599145
Rico <teh_programerer@interactivelaboratories.com>
2015-04-23 13:30:53 +0100

    Modified the Linux builds to be relocatable
    |BUILD CONFIGURATION|
    [build-ide.sh]
    * chrpath is used to change the rpath for the codeblocks executable in order to
      locate the libcodeblocks.so* files correctly
    [src/src/Makefile.am]
    * APP_PREFIX definition removed, to allow Code::Blocks to not rely on a set
      path for finding its executable directory

--- build-ide.sh
+++ build-ide.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 mkdir build
 cd build
-../configure --host=$HOSTMACH --target=$TARGETMACH --build=$BUILDMACH --prefix=$INSTALLDIR --with-wx-config=wx-config-2.8 --program-prefix=
+../configure --host=$HOSTMACH --target=$TARGETMACH --build=$BUILDMACH --with-wx-config=wx-config-2.8 --prefix=${INSTALLDIR} --program-prefix=
 make
 make install
+# rpath needs to be set for Code::Blocks to use the correct libcodeblocks.so
+chrpath -r '${ORIGIN}/../lib' ${INSTALLDIR}/bin/codeblocks
