524297eb2abbc0e85ee9eaeec36f3ed6c7d9dbdd
Rico <teh_programerer@interactivelaboratories.com>
2015-04-24 00:08:04 +0100

    Added a basic SEGA Saturn sample that loops endlessly
    |SOURCE|
    [src/plugins/scriptedwizard/resources/saturn/c/main.c]
    * A bare-bones example showing how to set up the Saturn and loop until power is
      killed
    [src/plugins/scriptedwizard/resources/saturn/Saturn.lnk]
    * A basic linker script for the SEGA Saturn
    
    |COMPILER CONFIGURATION|
    [src/plugins/compiler/gcc/resources/compilers/compiler_sh2-gcc.xml]
    [src/plugins/compiler/gcc/resources/compilers/options_sh2-gcc.xml]
    * Initial pass at the Hitachi SH-2 compiler

--- src/plugins/scriptedwizard/resources/saturn/c/main.c
+++ src/plugins/scriptedwizard/resources/saturn/c/main.c
@@ -1,8 +1,31 @@
-#include <stdio.h>
-#include <stdlib.h>
+#define SYSTEM_WORK	0x060FFC00
+#define SYSTEM_SIZE	( 0x06100000 - 0x060FFC00 )
 
-int main()
+extern unsigned int _BSSBegin, _BSSEnd;
+
+void main( void )
 {
-    printf("Hello world!\n");
-    return 0;
+	unsigned char *pDestination;
+	unsigned int Index;
+
+	/* Zero the .bss */
+	for( pDestination = ( unsigned char * )&_BSSBegin;
+		pDestination < ( unsigned char * )&_BSSEnd; ++pDestination )
+	{
+		*pDestination = 0;
+	}
+
+	/* This is for clearing the SGL system variables */
+	for( pDestination = ( unsigned char * )SYSTEM_WORK, Index = 0;
+		Index < SYSTEM_SIZE; ++Index )
+	{
+		*pDestination = 0;
+	}
+
+	while( 1 )
+	{
+		/* Just keep going */
+	}
 }
+
+
