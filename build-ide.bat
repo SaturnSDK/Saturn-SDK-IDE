bda09c97186fb8168f5ba2a25e744baa0c4b37d4
Rico Tyrell <rico@redringrico.com>
2015-07-05 00:56:32 +0100

    The Windows version of Saturn.lnk for SEGA Saturn Basic is correctly copied

--- build-ide.bat
+++ build-ide.bat
@@ -1,6 +1,9 @@
 @echo off
 cd src
+del cbp2make.cfg
 cbp2make --local --config show
+sed.exe -i "s/g++.exe/i686-w64-mingw32-g++.exe/g" cbp2make.cfg
+sed.exe -i "s/gcc.exe/i686-w64-mingw32-gcc.exe/g" cbp2make.cfg
 cbp2make --local -in CodeBlocks.cbp -out Makefile -windows -targets "All" --target-case keep
 sed.exe -i "s/\*/\\*/g" Makefile
 sed.exe -i -e "s/$(LIB)-l/$(LIB) -l/g" Makefile
